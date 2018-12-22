826b7961287677df7b20bc471f6d700509dc4838
jekitut <44940036+jekitut@users.noreply.github.com>
2018-12-22 11:07:22 +0700

    Add files via upload

---
+++ bot/caping/config.php
@@ -0,0 +1,34 @@
+<?php
+$config = [
+
+
+
+//"u=xxx;n=xxx", # Akun 1
+//"u=xxx;n=xxx", # Akun 2
+//"u=xxx;n=xxx", # Akun 3
+//"u=xxx;n=xxx", # Akun 4
+//"u=xxx;n=xxx", # Akun 5
+
+
+
+# Untuk Mendapatkan + 300 - 600 Poin
+# Masukkan Kode Undangan : jeitm4
+
+# Ganti Tanda (xxx) Dengan Data
+# Data Yang Ada Di Packet Capture
+
+# Hapus Tanda (//) Untuk Mengakifkan Akun
+
+# Akun 6 dan Seterusnya Bisa Di Buat Sendiri
+
+# Untuk Menyimpan Tekan Tombol
+# CTRL + X > Y > Enter
+
+
+
+# Sistem Bot Caping
+"u=62082665;n=0000000016568c240000000063f24fcc",
+"u=62193314;n=00000000220bf4c5b4b70b54338fbb97",
+"u=62192986;n=00000000220bf4c5b4b70b54338fbb97",
+"u=62078977;n=00000000220bf4c5b4b70b54338fbb97" // gmail
+];
