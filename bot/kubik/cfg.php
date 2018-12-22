2ebf8d1e345e7a20fa4b5b66c83a0602d4e78a61
jekitut <44940036+jekitut@users.noreply.github.com>
2018-12-22 11:08:52 +0700

    Add files via upload

---
+++ bot/kubik/cfg.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+$android_id = "37771a7198ab2f36";
+$deviceCode = "357559060490988";
+$device_ip  = "180.254.55.254";
+$token = "f029-ALXEalG7-ththpj-f0iOao0bzJ1Nw-Av8jEsNZTVvOu8oxllizlTg9JWEpxJPjzKFLgYh1sHzk";
+$tk = "ACB7NbhlcptNDLQz70IQSXPCz6QtNRvg7h9xdHRodw";
+$uuid = "49aeabdc6a964a1f89b6035366e95b31";
+$userid = "3292069";
+$sign = "15fc2f53bd3619b1b2c5c0c14036cb19";
+
+
+
+# Ganti Tanda (xxx) Dengan Data
+# Data Yang Ada Di Packet Capture
+
+# Authorization = $token
+
+# Untuk Menyimpan Tekan Tombol
+# CTRL + X > Y > Enter
