7e8e148be58a6cc88438a7aa8dd50d4c11771799
jekitut <44940036+jekitut@users.noreply.github.com>
2018-12-22 11:12:49 +0700

    Add files via upload

---
+++ bot/totonews/config.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+$config = [
+//"ticket=xxx&meid=xxx", # Akun 1
+//"ticket=xxx&meid=xxx", # Akun 2
+//"ticket=xxx&meid=xxx", # Akun 3
+//"ticket=xxx&meid=xxx", # Akun 4
+//"ticket=xxx&meid=xxx", # Akun 5
+
+
+
+# Untuk Mendapatkan + 300 - 600 Poin
+# Masukkan Kode Undangan :
+
+# Ganti Tanda (xxx) Dengan Data
+# Data Yang Ada Di Packet Capture
+
+# Hapus Tanda (//) Untuk Mengakifkan Akun
+
+# Akun 6 dan Seterusnya Bisa Di Buat Sendiri
+
+# Untuk Menyimpan Tekan Tombol
+# CTRL + X > Y > Enter
+
+
+
+# Sistem Bot ToToNews
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh4fbrtB-o7-1g8uwoJhpipyrzo7cl2S_m5Nnhn2vl3-HnZev042yv8t_y62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=0002be4023d15348",
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh4eVu9OJssDLlJSxaIpqip-uloPcfpiznnVlhnq3lYCHd927vY2vtKh2lq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=37771a7198ab2f36",
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh6LRurqjr7TOfpWxoHaxiobM2YLciWPAhYWrkI3Azn56h5a64IlowLh_z62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=37771a7198ab2f36",
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBin_cr62Jr7O4gpaxoJivfqyyk4K2m2XAhYFkkLCz2oudj96u4IahtaiKk62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=37771a7198ab2f36",
+  ];
+?>
