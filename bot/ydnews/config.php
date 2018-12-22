1f4b119a84e02a2ab54874cf7f0461791a5abc54
jekitut <44940036+jekitut@users.noreply.github.com>
2018-12-22 11:16:08 +0700

    Add files via upload

---
+++ bot/ydnews/config.php
@@ -0,0 +1,23 @@
+<?php
+$config = [
+"imei" => ["357559060490988",],
+"sign" => ["852ccc8ca5aa7b3510d622fa11ec7fa2faa079b6",],
+"token" => ["4dc0cac52bc2a4be9783146efa9c4e43",],
+"uuid" => ["b7d70682-df8d-44a2-a2c8-f2dc9132a75f",],
+];
+
+
+
+# Untuk Mendapatkan + 300 - 600 Poin
+# Masukkan Kode Undangan :
+
+# Ganti Tanda (xxx) Dengan Data
+# Data Yang Ada Di Packet Capture
+
+# Hapus Tanda (//) Untuk Mengakifkan Akun
+
+# Akun 6 dan Seterusnya Bisa Di Buat Sendiri
+
+# Untuk Menyimpan Tekan Tombol
+# CTRL + X > Y > Enter
+?>
